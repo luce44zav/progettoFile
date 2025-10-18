884a2fa385a1cf21d3b42382367f836aa6717112
Luce Zavaglia <LUCE.ZAVAGLIA@local.ittcesena.it>
2025-10-18 12:52:00 +0200

    versione stream writer

---
+++ StreamReader-Writer/StreamWriter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneFile
+{
+    internal class Writer
+    {
+        private string testo;
+        StreamWriter sw;
+
+
+        public Writer()
+        {
+            sw = new StreamWriter("Info.txt");
+        }
+
+        public void ScriviFile(string t)
+        {
+            testo = t;
+            sw.WriteLine(testo);
+            sw.Close();
+        }
+    }
+}
