c2a6ee082ea1a51089b39aead0dee8a603eca028
Luce Zavaglia <LUCE.ZAVAGLIA@local.ittcesena.it>
2025-10-24 09:37:42 +0200

    R0.1: completata la classe reader

--- StreamReader-Writer/StreamReader.cs
+++ StreamReader-Writer/StreamReader.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace StreamReader_Writer
 {
     internal class StreamReader
     {
+
+        private string filePath;
+
+        public StreamReader(string path)
+        {
+            filePath = path;
+        }
+
+        public void LeggiFile()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+
+                    while((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine("Errore nella lettura del file " + e.Message); 
+            }
+
+        }
+
     }
 }
